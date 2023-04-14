7e22183ca94e19fe0ce60879318ee31562563ccb
Philip Jiale Feng <106422796+PhilipJialeFeng@users.noreply.github.com>
2023-04-14 17:14:04 +1000

    changes

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,18 +1,53 @@
 var addressPoints = [
   [
+    "Sofia, Bulgaria",
+    42.6977028,
+    23.3217359
+  ],
+  [
+    "Cambridge, UK",
+    52.2055314,
+    0.1186637
+  ],
+  [
     "Barga, Italy",
     44.0739178,
     10.484263
   ],
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.272863
+    "Dubrovnik, Croatia",
+    42.64873625,
+    18.094680152605108
   ],
   [
-    "London, UK",
-    51.5073359,
-    -0.12765
+    "Zell am See, Austria",
+    47.3239636,
+    12.7963165
+  ],
+  [
+    "Hasselt, Belgium",
+    50.9303735,
+    5.3378043
+  ],
+  [
+    "Zhejiang, China",
+    29.0000001,
+    119.9999999
+  ],
+  [
+    "Leura, Australia",
+    -33.7156888,
+    150.3317665
+  ],
+  [
+    "Brisbane, Australia",
+    -27.4689682,
+    153.0234991
+  ],
+  [
+    "Lorne, Australia",
+    -38.5411691,
+    143.9748166
   ],
   [
     "Sydney, Australia",
